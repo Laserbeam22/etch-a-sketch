7a53b07e5d4446e669e652f67a84c9beed6bb071
Melissa <melissa.layser22@gmail.com>
2022-09-12 22:17:55 -0700

    Create func loop for div creation w/ existing vars

--- javascript.js
+++ javascript.js
@@ -1,2 +1,18 @@
-const container = document.querySelector('#container');
-const singleDiv = document.createElement('div'); 
+//Takes a number parameter & loops for creating new divs 
+
+function createDivs(num) {
+    
+    const container = document.querySelector('#container');
+
+
+    for (let i = 0; i < num; i++) {
+        singleDiv = document.createElement('div');
+        container.appendChild(singleDiv);
+        singleDiv.textContent = "I'm a div.";
+        singleDiv.style.cssText = 'background: pink; border: solid #000000';
+        
+    }
+    return singleDiv;
+}
+
+console.log(createDivs(256));
