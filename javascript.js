d18810efee48f3a3e2f52241615dc85af671ea3a
Melissa <melissa.layser22@gmail.com>
2022-09-22 14:32:48 -0700

    Add event listener w/ prompt func to btn

--- javascript.js
+++ javascript.js
@@ -13,4 +13,29 @@ function createDivs(num) {
     })
   }
 }
-  console.log(createDivs(256));
+
+
+btn.addEventListener('click', function (e) {
+  
+  let num = getNum();
+
+  function getNum() {
+
+    let input = prompt("Choose a number from 3-100 for sketch dimensions.");
+    let notString = parseInt(input);
+    let squared = notString*notString;
+    
+    if (Number.isInteger(notString) == true && notString >= 3 && notString <=100) {
+        return squared;
+    } else {
+        return("Error. Please choose a number from 3-100.");
+    }
+  }
+  createDivs(num);
+});
+
+
+
+//console.log(createDivs(num));
+
+
