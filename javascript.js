969c9db91d97903294389650362e675beb3cd2fc
Melissa <melissa.layser22@gmail.com>
2022-09-26 16:04:31 -0700

    Add doc. frag. & mv eventListener to reduce reflow

--- javascript.js
+++ javascript.js
@@ -1,23 +1,26 @@
 //Takes a number parameter & loops for creating new divs 
 
 function createDivs(num) {
+  var dFrag = document.createDocumentFragment();
   for (i = 0; i < num; i++) {
     const singleDiv = document.createElement('div');
     singleDiv.classList.add('singleDiv');
-    container.appendChild(singleDiv);
-
-    /*document.querySelectorAll('.singleDiv').forEach(item => {
-      item.addEventListener('mouseenter', event => {
-        event.target.style.backgroundColor = 'rgb(70, 150, 231)';
-      })
-    })*/
+    //container.appendChild(singleDiv);
+    dFrag.appendChild(singleDiv);
   }
+  container.appendChild(dFrag);
+  document.querySelectorAll('.singleDiv').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+      event.target.style.backgroundColor = 'rgb(70, 150, 231)';
+    })
+  })
 }
 
 btn.addEventListener('click', function (e) {
   //resets child div amount to new given number 
   container.replaceChildren();
 
+  //Function that prompts for a number, then squares the number for the proper amount of divs to create the grid
   let num = getNum();
 
   function getNum() {
@@ -26,7 +29,6 @@ btn.addEventListener('click', function (e) {
     let notString = parseFloat(input);
     let squared = notString*notString;
     const grid = document.getElementById('container');
-    //repeatNumber = notString;
     
     if (Number.isInteger(notString) !== true || notString < 4 || notString > 100) {
       alert("Error. Please choose a number from 4-100.");  
